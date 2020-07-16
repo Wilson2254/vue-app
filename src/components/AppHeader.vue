1e8ba49b512fe7b4fefa3497244318c210f529dd
Alexander_P <sanek191296@mail.ru>
2020-07-16 18:12:38 +0300

    create navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,20 +1,38 @@
 <template>
-<v-app>
-  <v-app-bar app dark class="primary">
+  <v-app>
+    <v-app-bar app dark class="primary">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon >
       <v-toolbar-title v-text="'English Speak'"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items class="hidden-sm-and-down">
         <v-btn v-for="(items, item) in menuItems" :key="item" :to="items.route" text>
-            <v-icon left v-html="items.icon"> </v-icon>
-            {{items.title}}
+          <v-icon left v-html="items.icon"></v-icon>
+          {{items.title}}
         </v-btn>
       </v-toolbar-items>
-  </v-app-bar>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary class="hidden-md-and-up">
+      <v-list nav dense>
+        <v-list-item-group v-model="group">
+          <v-list-item v-for="(items, item) in menuItems" :key="item" :to="items.route" text>
+            <v-icon left v-html="items.icon"></v-icon>
+            {{items.title}}
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </v-app>
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      drawer: false,
+      group: null
+    };
+  },
   computed: {
     menuItems() {
       return [
@@ -49,6 +67,11 @@ export default {
           route: "/signup"
         }
       ];
+    }
+  },
+  watch: {
+    group() {
+      this.drawer = false;
     }
   }
 };
