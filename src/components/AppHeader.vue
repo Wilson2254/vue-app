52794a6b82f664c80a1c1f6a40e1ca9ef8594f4e
Alexander_P <sanek191296@mail.ru>
2020-07-17 04:08:41 +0300

    rename title

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -47,7 +47,7 @@ export default {
             },
             {
               icon: "mdi-account",
-              title: "Кабинет",
+              title: "Личный кабинет",
               route: "/profile"
             },
             {
